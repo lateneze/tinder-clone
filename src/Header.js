7380411675dd54193b7ec07bcc6a0850bd0862b9
lateneze <lateneze@gmail.com>
2021-12-18 02:06:50 +0000

    Some few updates

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@mui/icons-material/Person";
+import { IconButton } from "@mui/material";
+import ForumIcon from "@mui/icons-material/Forum";
+
+function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+      <img
+        className="header_logo"
+        src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+        alt=""
+      />
+      <IconButton>
+        <ForumIcon className="header_icon" fontSize="large" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default Header;
